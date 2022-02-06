562242d1642f64e715a5d13f310af13a6c380b85
MrDogeBro <MrDogeBro@users.noreply.github.com>
2022-02-05 22:23:34 -0500

    Auto formatting and add basic direction of where users should go

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import Head from 'next/head';
-import { Component } from 'react';
+import Head from "next/head";
+import { Component } from "react";
 
-import { Navbar } from '@ui/Navbar';
-import { Footer } from '@ui/Footer';
+import { Navbar } from "@ui/Navbar";
+import { Footer } from "@ui/Footer";
 
 type Props = {};
 type States = {};
@@ -23,7 +23,13 @@ export default class Home extends Component<Props, States> {
         </Head>
         <div className="flex flex-col min-h-screen">
           <Navbar />
-          <main className="flex-grow"></main>
+          <main className="flex-grow">
+            <h1 className="text-2xl text-white text-center mt-20 px-6">
+              If you are the base computer, please head over to base page.
+              Otherwise please head over to the user page where you can connect
+              to the comms. Thanks!
+            </h1>
+          </main>
           <Footer />
         </div>
       </div>
