169bbd636232a154b0f38ec14501deada7c09c5b
MrDogeBro <MrDogeBro@users.noreply.github.com>
2022-02-07 22:29:04 -0500

    Add config for remote firestore and make config env vars

--- src/firestore.ts
+++ src/firestore.ts
@@ -1,10 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  projectId: "opencomm-local",
-  databaseURL: "http://localhost:9000/?ns=opencomm-local",
-};
+let firebaseConfig;
+
+if (process.env.NEXT_PUBLIC_FIREBASE_REMOTE_FIRESTORE == "true")
+  firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  };
+else
+  firebaseConfig = {
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    databaseURL: "http://localhost:9000/?ns=opencomm-local",
+  };
 
 const firebase = initializeApp(firebaseConfig);
 
