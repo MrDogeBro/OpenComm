2bfceff5a2a73e7013a3e94af346fa560a45008c
MrDogeBro <MrDogeBro@users.noreply.github.com>
2022-05-12 09:25:19 -0400

    Add user selection and take in user from query

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,9 +1,12 @@
 import Head from "next/head";
+import { NextPageContext } from "next";
+import { withRouter, NextRouter } from "next/dist/client/router";
 import { Component } from "react";
 
 import { Navbar } from "@ui/Navbar";
 import { Footer } from "@ui/Footer";
 import { Button } from "@ui/Button";
+import { User as UserCard, UserType } from "@ui/User";
 
 import { Mic } from "@utils/mic";
 
@@ -16,12 +19,15 @@ import {
   onSnapshot,
 } from "firebase/firestore";
 
-type Props = {};
+type Props = { query: QueryProps; router: NextRouter };
+type QueryProps = { userName: string; userId: string };
+
 type States = {
   muted: boolean[];
   listening: boolean[];
   started: boolean;
   remoteStreamUpdate: boolean;
+  user: UserType;
 };
 
 class User extends Component<Props, States> {
@@ -29,6 +35,11 @@ class User extends Component<Props, States> {
   private localStream: MediaStream | null = null;
   private remoteStream: MediaStream | null = null;
   private numStreams: number;
+
+  private users = [
+    { name: "Testing User", id: "2340df" },
+    { name: "test2 user test", id: "234dfkdfg" },
+  ];
 
   constructor(props: Props) {
     super(props);
@@ -40,12 +51,29 @@ class User extends Component<Props, States> {
       listening: [],
       started: false,
       remoteStreamUpdate: true,
+      user: { name: "", id: "" },
     };
 
     if (typeof window !== "undefined") {
       this.conn = new RTCPeerConnection();
     }
   }
+
+  static getInitialProps({ query }: NextPageContext) {
+    return { query };
+  }
+
+  componentDidMount = () => {
+    let query = this.props.query;
+
+    if (query.userId || query.userName) {
+      this.users.forEach((user) => {
+        if (user.id == query.userId) this.setState({ user: user });
+        else if (user.name == query.userName) this.setState({ user: user });
+      });
+      this.handleSetup().then(this.handleStart);
+    }
+  };
 
   handleSetup = async () => {
     this.localStream = await navigator.mediaDevices.getUserMedia({
@@ -143,23 +171,40 @@ class User extends Component<Props, States> {
               playsInline
             ></audio>
 
-            <div className="flex justify-center pt-16 ">
-              <Button
-                onClick={() => {
-                  this.handleSetup().then(this.handleStart);
-                }}
-                color="primary"
-                disabled={this.state.started}
-              >
-                Start
-              </Button>
+            <div
+              className={`pt-16 mx-auto w-2/4 ${
+                this.state.user.id.length > 0 ? "hidden" : null
+              }`}
+            >
+              {this.users.map((user) => {
+                return (
+                  <UserCard
+                    name={user.name}
+                    className="mb-4"
+                    onClick={() => {
+                      this.setState({ user: user });
+                      this.props.router.push(
+                        `user?userName=${user.name}&userId=${user.id}`
+                      );
+                      this.handleSetup().then(this.handleStart);
+                    }}
+                    key={user.id}
+                  />
+                );
+              })}
+            </div>
+
+            <div className="flex justify-center">
+              <span className="text-white text-center text-xl mt-16">
+                {this.state.user.name}
+              </span>
             </div>
 
             <div className="grid gap-4 grid-flow-col justify-center pt-6">
               {this.localStream?.getAudioTracks().map((track, index) => {
                 return (
-                  <div>
-                    <h1 key={index + 1} className="text-white p-3 text-center">
+                  <div key={index + 1}>
+                    <h1 className="text-white p-3 text-center">
                       Channel {index + 1}
                     </h1>
                     <Button
@@ -179,7 +224,6 @@ class User extends Component<Props, States> {
                       disabled={!this.state.started}
                       color="primary"
                       className="w-full"
-                      key={index}
                     >
                       {this.state.muted[index] ? "Unmute" : "Mute"}
                     </Button>
@@ -191,8 +235,8 @@ class User extends Component<Props, States> {
             <div className="grid gap-4 grid-flow-col justify-center pt-6">
               {this.remoteStream?.getAudioTracks().map((track, index) => {
                 return (
-                  <div>
-                    <h1 key={index + 1} className="text-white p-3 text-center">
+                  <div key={index + 1}>
+                    <h1 className="text-white p-3 text-center">
                       Channel {index + 1}
                     </h1>
                     <Button
@@ -212,7 +256,6 @@ class User extends Component<Props, States> {
                       disabled={!this.state.started}
                       color="secondary"
                       className="w-full"
-                      key={index}
                     >
                       {this.state.listening[index]
                         ? "Stop Listening"
@@ -230,4 +273,4 @@ class User extends Component<Props, States> {
   }
 }
 
-export default User;
+export default withRouter(User);
